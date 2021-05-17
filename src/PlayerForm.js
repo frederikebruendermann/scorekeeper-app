6b615d70638bff114ae08f1567c6d66d5ca30edb
Frederike Bründermann <82148810+frederikebruendermann@users.noreply.github.com>
2021-05-17 14:50:07 +0200

    add PlayerForm

---
+++ src/PlayerForm.js
@@ -0,0 +1,26 @@
+import './PlayerForm.css'
+import PropTypes from 'prop-types'
+
+Playerform.propTypes = {
+  onSubmit: PropTypes.func,
+}
+
+export default function Playerform({ onSubmit }) {
+  return (
+    <form className="PlayerForm" onSubmit={handleSubmit}>
+      <label>
+        Add Player:
+        <input name="name" type="text" placeholder="Player name" />
+      </label>
+    </form>
+  )
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = event.target
+    const input = form.elements.name
+    onSubmit(input.value)
+    form.reset()
+    input.focus()
+  }
+}
---
+++ src/Playerform.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PlayerForm from './PlayerForm'
+
+export default {
+  title: 'PlayerForm',
+  component: PlayerForm,
+}
+
+const Template = args => <PlayerForm {...args} />
+export const DefaultForm = Template.bind({})
+DefaultForm.args = {}
