0f9f3b699ac6b4ee76d75e559f42065304f1ebe9
Frederike Bründermann <82148810+frederikebruendermann@users.noreply.github.com>
2021-05-18 12:00:39 +0200

    create app with components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Player from './Player'
+import PlayerForm from './PlayerForm'
+import Button from './Button'
+
 function App() {
-  return <div className="App">Please open Storybook</div>
+  const [players, setPlayers] = useState([
+    { name: 'John Doe', score: 20 },
+    { name: 'Jane Doe', score: 120 },
+  ])
+
+  return (
+    <div className="App">
+      {players.map(player => (
+        <Player key={player.name} name={player.name} score={player.score} />
+      ))}
+
+      <Button>Reset score</Button>
+      <Button>Reset all</Button>
+      <PlayerForm onSubmit={createPlayer} />
+    </div>
+  )
+
+  function createPlayer(name) {
+    setPlayers([...players, { name, score: 0 }])
+  }
 }
 
 export default App
